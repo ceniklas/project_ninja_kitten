e57ce3688b035bfd1314a9dd5b91a99c2261298e
jannaha <jsson.hanna@gmail.com>
2014-09-24 16:05:04 +0200

    Added som obstacles to the chinesewall

---
+++ Assets/Scripts/ObstaclePlacer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstaclePlacer : MonoBehaviour {
+
+	public GameObject jumpObstacle;
+	public GameObject slideObstacle;
+	public GameObject obstacle;
+	private int nrOfObstacles = 10;
+	private int rndNr;
+
+	// Use this for initialization
+	void Start () {
+		for (int n = 1; n < nrOfObstacles; n++) {
+		rndNr = (int)(Random.value + 0.5);
+
+			if (rndNr == 0) {
+				obstacle = jumpObstacle;
+			}
+			else{
+				obstacle = slideObstacle;
+			}
+			Instantiate(obstacle, new Vector3(3.3f, 1.8f, n * 20.0f), Quaternion.identity);
+		}	
+	}
+}
