8315f47545361ab64f97077b2b7a6736251dfa23
jannaha <jsson.hanna@gmail.com>
2014-10-08 17:16:38 +0200

    Added a startmenu and a pausmenu

---
+++ Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuHandler : MonoBehaviour {
+
+	private Color defaultColor = new Color32(255,45,195,255);
+	public bool gameQuit = false;
+
+	void OnMouseEnter(){
+		renderer.material.color = Color.black;
+	}
+
+	void OnMouseExit(){
+		renderer.material.color = defaultColor;
+	}
+
+	void OnMouseDown(){
+		if (gameQuit) {
+			Application.Quit();
+		}
+		else{
+			Application.LoadLevel(1);
+		}
+
+	}
+}
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour {
+
+	public GUISkin pauseSkin;
+	private Rect pauseRect;
+	private bool ifPaused = false;
+
+	// Use this for initialization
+	void Start () {
+		//Rect(float left, float top, float width, float height);
+		pauseRect = new Rect (Screen.width * 0.5f - 100, Screen.height * 0.5f - 100, 200, 200);
+	}
+
+	// Update is called once per frame
+	void Update (){
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+
+			//if the game is paused, unpause it
+			if(ifPaused){
+				ifPaused = false;
+				Time.timeScale = 1;
+			}
+			//pause the game
+			else{
+				ifPaused = true;
+				Time.timeScale = 0;
+			}
+		}
+	}
+
+	void OnGUI(){
+		if (ifPaused){
+			pauseRect = GUI.Window(0, pauseRect, pauseFunc, "Pause Menu");
+		}
+	}
+
+	void pauseFunc(int id){
+		if (GUILayout.Button ("Resume Game")){
+			ifPaused = false;
+			Time.timeScale = 1;
+		}
+
+	}
+}
