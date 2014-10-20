25b55d28c943dcc5b209ede7d7b23750d9c8d278
unknown <jsson.hanna@gmail.com>
2014-10-20 22:10:24 +0200

    Fixing the superpoints

---
+++ Assets/Scripts/FirstSuperPoint.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirstSuperPoint : MonoBehaviour {
+
+	public GUISkin tutoringSkin;
+	public Texture2D phone;
+	public GameObject superPoint;
+	private GameObject sp;
+	private Rect superRect;
+	private float timer;
+	private Vector3 firstPos;
+	
+	bool displayInfo = false;
+	string info = "You picked up  \n a SuperPoint! \n \n Superpoints gives you \n an extra boost! \n \n But be careful, \n your boost is lost \n if you miss one!";
+
+	void Start () {
+		superRect = new Rect (Screen.width * 0.5f - 250, Screen.height * 0.5f - 400, 500, 500);
+
+		firstPos = new Vector3 (0,0,-370);
+
+		GameObject sp = (GameObject)Instantiate (superPoint, firstPos, Quaternion.identity);
+		sp.transform.parent = transform;
+	}
+	
+	void OnTriggerEnter(Collider c){
+		if (c.tag == "Player") {
+			displayInfo = true;
+			Time.timeScale = 0;
+		}
+	}
+
+	void OnGUI(){
+		GUI.skin = tutoringSkin;
+		
+		if (displayInfo){
+			GUI.TextField(superRect, info);
+			if(GUI.Button(superRect, "Knapp")){
+				displayInfo = false;
+				Time.timeScale = 1;
+			}
+		}
+	}
+
+}
---
+++ Assets/Scripts/SuperPointPlacer.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SuperPointPlacer : MonoBehaviour {
+
+	public GameObject superPoint;
+	private GameObject superPlacer;
+	private int maximumPoints = 5;
+	private int distanceBetween = 3;
+	private int start = 0;
+	private Vector3 pointPosition, firstSuperPoint;
+	private int xPos;
+	private int margin = 6;
+	private int[] posArray;
+	private const int size = 22;
+	
+	// Use this for initialization
+	void Start () {
+		posArray = new int[size]{4,-4,0,4,4,-4,-4,4,-4,4,-4,-4,4,0,0,4,-4,4,0,0,0,0};
+
+		firstSuperPoint = new Vector3 (0,0, -370);
+
+		GameObject superPlacer = (GameObject)Instantiate (superPoint, firstSuperPoint, Quaternion.identity);
+		superPlacer.transform.parent = transform;
+		
+		for (int n=0; n<size; n++) {
+			xPos = posArray [n];
+			start = createRowOfCoins (xPos);
+		}
+	}
+	
+	int createRowOfCoins(int x){
+		int newStart = 0;
+		for (int i=start; i<maximumPoints + start; i++) {
+			
+			pointPosition = new Vector3(x,0, (i * distanceBetween));
+			
+			GameObject superPlacer = (GameObject)Instantiate (superPoint, pointPosition, Quaternion.identity);
+			superPlacer.transform.parent = transform;
+			
+			newStart = i; 	                        
+		}
+		newStart += margin;
+		
+		return newStart;
+	}
+
+}
